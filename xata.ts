7852ca94a38461f01f540687a1d3be7303d9cfe6
Muthonikiboi <kiboijoye254@gmail.com>
2024-10-16 11:13:04 +0300

    ADD:DB-Schemas

--- xata.ts
+++ xata.ts
@@ -9,6 +9,98 @@ require('dotenv').config();
 
 const tables = [
   {
+    name: "Blogs",
+    checkConstraints: {
+      Blogs_xata_id_length_xata_id: {
+        name: "Blogs_xata_id_length_xata_id",
+        columns: ["xata_id"],
+        definition: "CHECK ((length(xata_id) < 256))",
+      },
+    },
+    foreignKeys: {},
+    primaryKey: [],
+    uniqueConstraints: {
+      _pgroll_new_Blogs_xata_id_key: {
+        name: "_pgroll_new_Blogs_xata_id_key",
+        columns: ["xata_id"],
+      },
+    },
+    columns: [
+      {
+        name: "body",
+        type: "text",
+        notNull: true,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "description",
+        type: "text",
+        notNull: true,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "images",
+        type: "text",
+        notNull: true,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "tag",
+        type: "text",
+        notNull: false,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "title",
+        type: "text",
+        notNull: true,
+        unique: false,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "xata_createdat",
+        type: "datetime",
+        notNull: true,
+        unique: false,
+        defaultValue: "now()",
+        comment: "",
+      },
+      {
+        name: "xata_id",
+        type: "text",
+        notNull: true,
+        unique: true,
+        defaultValue: "('rec_'::text || (xata_private.xid())::text)",
+        comment: "",
+      },
+      {
+        name: "xata_updatedat",
+        type: "datetime",
+        notNull: true,
+        unique: false,
+        defaultValue: "now()",
+        comment: "",
+      },
+      {
+        name: "xata_version",
+        type: "int",
+        notNull: true,
+        unique: false,
+        defaultValue: "0",
+        comment: "",
+      },
+    ],
+  },
+  {
     name: "Users",
     checkConstraints: {
       Users_xata_id_length_xata_id: {
@@ -20,12 +112,48 @@ const tables = [
     foreignKeys: {},
     primaryKey: [],
     uniqueConstraints: {
+      Users__pgroll_new_email_key: {
+        name: "Users__pgroll_new_email_key",
+        columns: ["email"],
+      },
+      Users__pgroll_new_password_key: {
+        name: "Users__pgroll_new_password_key",
+        columns: ["password"],
+      },
+      Users__pgroll_new_username_key: {
+        name: "Users__pgroll_new_username_key",
+        columns: ["username"],
+      },
       _pgroll_new_Users_xata_id_key: {
         name: "_pgroll_new_Users_xata_id_key",
         columns: ["xata_id"],
       },
     },
     columns: [
+      {
+        name: "email",
+        type: "text",
+        notNull: true,
+        unique: true,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "password",
+        type: "text",
+        notNull: true,
+        unique: true,
+        defaultValue: null,
+        comment: "",
+      },
+      {
+        name: "username",
+        type: "text",
+        notNull: true,
+        unique: true,
+        defaultValue: null,
+        comment: "",
+      },
       {
         name: "xata_createdat",
         type: "datetime",
@@ -65,18 +193,22 @@ const tables = [
 export type SchemaTables = typeof tables;
 export type InferredTypes = SchemaInference<SchemaTables>;
 
+export type Blogs = InferredTypes["Blogs"];
+export type BlogsRecord = Blogs & XataRecord;
+
 export type Users = InferredTypes["Users"];
 export type UsersRecord = Users & XataRecord;
 
 export type DatabaseSchema = {
+  Blogs: BlogsRecord;
   Users: UsersRecord;
 };
 
 const DatabaseClient = buildClient();
 
 const defaultOptions = {
-    databaseURL: process.env.databaseURL,
-    apiKey: process.env.XATA_API_KEY,
+  databaseURL: process.env.databaseURL,
+  apiKey: process.env.XATA_API_KEY,
 };
 
 export class XataClient extends DatabaseClient<DatabaseSchema> {
